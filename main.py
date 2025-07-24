468b013a7444671c5bfa8dcf3e6bb0b664d70e5c
Marcelo Rizzi <mrizzi48@gmail.com>
2025-07-24 17:52:38 -0300

    Added type hints, code reviews and error handling

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import argparse
-import glob
 import json
 import logging
 from ftplib import FTP
@@ -10,37 +9,39 @@ from prompt_toolkit import prompt
 from mp3_util import MP3Transformer
 
 
-def move_files(mp3s: list, destination_folder_path: str) -> list:
+def move_files(mp3s: list[Path], destination_folder_path: str) -> list[str]:
 
     new_file_paths = []
     logger.info(f"Attempting to move {len(mp3s)} file(s).")
     for mp3f in mp3s:
-        new_file_name = Path(destination_folder_path) / Path(mp3f).name
+        new_file_name = Path(destination_folder_path) / mp3f.name
         logger.info(f"Moving file to destination: {new_file_name}")
-        Path(mp3f).rename(new_file_name)
-        new_file_paths.append(new_file_name)
+        mp3f.rename(new_file_name)
+        new_file_paths.append(str(new_file_name))
         
     return new_file_paths
 
-def send_to_phone(url:str, port:str, list_of_files) -> None:
+def send_to_phone(url:str, port:str, list_of_files:list[str]) -> None:
 
     ftp = FTP()
-    ftp.connect(url, int(port)) # Error handling needed
-    ftp.login('android', 'android')
+    try:
+        ftp.connect(url, int(port))
+        ftp.login('android', 'android')
+    except Exception as e:
+        logger.error(f"Failed to connect to FTP server at {url}:{port}. Error: {e}")
+        return
 
     for file in list_of_files:
         logger.info(f"Sending {file}")
         with open(file, 'rb') as f:
             ftp.storbinary(f'STOR Music/{Path(file).name}', f)
-    
-    quit()
 
-def save_cache(dict, cache_path, server_address, port) -> None:
-    if dict.get("server_address", "") != server_address or dict.get("port", "") != port:
-        dict["server_address"] = server_address
-        dict["port"] = port
-    with Path(cache_path).open('w', encoding='utf-8') as f:
-        json.dump(dict, f)
+def save_cache(data:dict, cache_path:Path, server_address:str, port:str) -> None:
+    if data.get("server_address", "") != server_address or data.get("port", "") != port:
+        data["server_address"] = server_address
+        data["port"] = port
+    with cache_path.open('w', encoding='utf-8') as f:
+        json.dump(data, f)
 
 if __name__ == "__main__":
 
@@ -63,20 +64,19 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Logging set-up according to arguments
-    logger = logging.getLogger(__name__)
-    logging_level = 0
-    if args.verbose:
-        logging_level = logging.DEBUG
+    logging_level = logging.DEBUG if args.verbose else logging.WARNING
     logging.basicConfig(level=logging_level)
+    logger = logging.getLogger(__name__)
 
     # MP3 Changer
-    list_of_mp3s = glob.glob(rf'{downloads_folder_path}\*.mp3')
+    list_of_mp3s = list(downloads_folder_path.glob("*.mp3"))
     mp3_transformer = MP3Transformer(list_of_mp3s, logging_level)
     mp3_transformer.change_attributes(str(cover_art_path))
     
     # Move files to folder locally and to phone
     new_file_paths = move_files(list_of_mp3s, str(destination_folder_path))
-    url = prompt("Please start your FTP server and enter your IP: ", default=cache.get("server_address", ""))
-    port = prompt("Please specify the opened port for your FTP server: ", default=cache.get("port", ""))
-    save_cache(cache, str(cache_path), url, port)
-    send_to_phone(url, port, new_file_paths)
+    if new_file_paths:
+        url = prompt("Please start your FTP server and enter your IP: ", default=cache.get("server_address", ""))
+        port = prompt("Please specify the opened port for your FTP server: ", default=cache.get("port", ""))
+        save_cache(cache, cache_path, url, port)
+        send_to_phone(url, port, new_file_paths)
--- mp3_util.py
+++ mp3_util.py
@@ -1,25 +1,26 @@
 import logging
-import os
+from pathlib import Path
 
 from mutagen.easyid3 import EasyID3
 from mutagen.id3 import APIC, ID3, PictureType  # type: ignore
 from mutagen.mp3 import MP3
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.WARNING)
 
 class MP3Transformer():
 
-    def __init__(self, list_of_mp3s, logger_level = 0):
+    def __init__(self, list_of_mp3s: list[Path], logger_level: int = 0) -> None:
         self.list_of_mp3s = list_of_mp3s
-        self.logger = logging.getLogger(__name__)
         logging.basicConfig(level=logger_level)
 
-    def change_attributes(self, cover_art_path: str):
+    def change_attributes(self, cover_art_path: str) -> None:
 
-        self.logger.info(f"Attempting to change attributes of {len(self.list_of_mp3s)} file(s).")
+        logger.info(f"Attempting to change attributes of {len(self.list_of_mp3s)} file(s).")
         for mp3f in self.list_of_mp3s:
 
-            self.logger.info(f"Changing text attributes of file {os.path.basename(mp3f)}.")
-            mp3File = MP3(mp3f, ID3=EasyID3)
+            logger.info(f"Changing text attributes of file {mp3f.name}.")
+            mp3File = MP3(str(mp3f), ID3=EasyID3)
             mp3File['album'] = 'Otros'
             mp3File['albumartist'] = 'Otros'
             mp3File['genre'] = 'Other'
@@ -27,27 +28,28 @@ class MP3Transformer():
             self.remove_attribute(mp3File, 'date')
             self.remove_attribute(mp3File, 'tracknumber')
 
-            self.logger.info("Saving file.")
+            logger.info("Saving file.")
             mp3File.save()
 
             # # Change cover art
-            audio = MP3(mp3f, ID3=ID3)
+            audio = MP3(str(mp3f), ID3=ID3)
             
             # Add new
-            self.logger.info("Deleting old cover art.")
+            logger.info("Deleting old cover art.")
             if audio.tags is not None:
                 audio.tags.delall("APIC")
-            self.logger.info("Adding new cover art.")
+            logger.info("Adding new cover art.")
             with open(cover_art_path, "rb") as art:
                 apic = APIC(data=art.read(), type=PictureType.COVER_FRONT, desc='Cover', mime="img/jpeg")
-                audio['APIC:'] = apic
+                audio['APIC'] = apic
 
             audio.save()
     
-    def remove_attribute(self, mp3, attribute_name):
-        self.logger.info(f"Attempting to remove {attribute_name}.")
+    @staticmethod
+    def remove_attribute(mp3: MP3, attribute_name:str) -> None:
+        logger.info(f"Attempting to remove {attribute_name}.")
         try:
             mp3.pop(attribute_name)
         except Exception as e:
-            self.logger.error(e)
-            self.logger.info(f"Failed to remove {attribute_name}.")
+            logger.error(e)
+            logger.info(f"Failed to remove {attribute_name}.")
